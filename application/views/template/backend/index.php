0a21629dcb48ba6b78d6fd52a8c3da91ac0e245f
ryifkiyidan <62694926+ryifkiyidan@users.noreply.github.com>
2021-03-17 22:27:50 +0700

    fix insert bug (jquery error)

--- application/views/template/backend/index.php
+++ application/views/template/backend/index.php
@@ -64,7 +64,7 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+    
     <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
@@ -74,12 +74,12 @@
     <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="<?= base_url('assets/'); ?>vendor/chart.js/Chart.min.js"></script>
+    <!-- <script src="<?php //echo base_url('assets/'); ?>vendor/chart.js/Chart.min.js"></script> -->
 
     <!-- Page level custom scripts -->
-    <script src="<?= base_url('assets/'); ?>js/demo/chart-area-demo.js"></script>
-    <script src="<?= base_url('assets/'); ?>js/demo/chart-pie-demo.js"></script>
-    <script src=" <?php echo base_url('assets/js/fontawesome.js');?> "></script>
+    <!-- <script src="<?php //echo base_url('assets/'); ?>js/demo/chart-area-demo.js"></script> -->
+    <!-- <script src="<?php //echo base_url('assets/'); ?>js/demo/chart-pie-demo.js"></script> -->
+    <script src="<?= base_url('assets/');?>js/fontawesome.js"></script>
     <script type = "text/javascript">
         $( document ).ready(function() {
             $("#nav-<?php echo $curr_page; ?>").addClass('active');
