33966b5b4fb2d515d08e42b385d5bc56804fa5ce
ryifkiyidan <62694926+ryifkiyidan@users.noreply.github.com>
2021-04-07 23:29:14 +0700

    delete file yang tidak diperlukan

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -16,14 +16,6 @@ class Auth extends MY_Controller
 		$this->render_login('login');
 	}
 
-	public function register_page()
-	{
-		if ($this->session->userdata('authenticated'))
-			redirect('page/dashboard');
-
-		$this->render_login('register');
-	}
-
 	public function login()
 	{
 
@@ -61,48 +53,6 @@ class Auth extends MY_Controller
 
 				redirect('page/dashboard');
 			}
-		}
-	}
-
-	public function register()
-	{
-		$iFName   = $this->input->post('iFName');
-		$iLName   = $this->input->post('iLName');
-		$iGender  = $this->input->post('iGender');
-		$iBDate   = $this->input->post('iBDate');
-		$username = $this->input->post('username');
-		$password = md5($this->input->post('password'));
-		$confirm_password = md5($this->input->post('confirm_password'));
-
-		$data     = $this->UserModel->get($username);
-		if (!empty($data['user'])) {
-			$this->session->set_flashdata('message', 'Username sudah ada');
-			redirect('auth/register_page');
-		}
-		if ($password !== $confirm_password) {
-			$this->session->set_flashdata('message', 'Confirm Password salah');
-			redirect('auth/register_page');
-		}
-		if (strlen($password) < 6) {
-			$this->session->set_flashdata('message', 'Password harus mengandung 6 atau lebih karakter');
-			redirect('auth/register_page');
-		}
-
-		$data = array(
-			'first_name'      => $iFName,
-			'last_name'       => $iLName,
-			'gender'          => $iGender,
-			'birth_date'      => $iBDate,
-			'username'        => $username,
-			'password'        => $password
-		);
-
-		if ($this->UserModel->registerAccount($data)) {
-			$this->session->set_flashdata('message', 'Akun berhasil dibuat, Silahkan Login');
-			redirect('auth');
-		} else {
-			$this->session->set_flashdata('message', 'Akun gagal dibuat, Silahkan Coba Beberapa Saat Lagi');
-			redirect('auth');
 		}
 	}
 
