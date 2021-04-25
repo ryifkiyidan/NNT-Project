30268717adacb2fa55c7baca88063bdaf960fb59
ryifkiyidan <62694926+ryifkiyidan@users.noreply.github.com>
2021-04-25 15:29:57 +0700

    Fix DatabaseModel

--- application/models/DatabaseModel.php
+++ application/models/DatabaseModel.php
@@ -141,7 +141,7 @@ class DatabaseModel extends CI_Model
 			->from('product as p')
 			->join('fabric as f', 'p.ID_Fabric = f.ID')
 			->group_by('f.Name')
-			->limit(5)
+			->limit(4)
 			->order_by('Total', 'DESC')
 			->get()->result();
 		return $data;
